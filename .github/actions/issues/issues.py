e83d37ffb205b99050aa28f4de041d3470f06dea
promising-candidate <41706714+promising-candidate@users.noreply.github.com>
2026-03-21 10:02:37 -0500

    Create issues.py

---
+++ .github/actions/issues/issues.py
@@ -0,0 +1,31 @@
+import requests
+import os
+import json
+
+
+def create_issue(title, body, assignees, milestone=None, labels='bug', repo=None):
+    base_  = 'https://api.github.com/repos/promising-candidate/' + repo + '/issues'
+    token = os.environ['PAT']
+    headers = {'Authorization': 'Bearer ' + token}
+    data_dict = {'title':title,
+                 'body':body,
+                 'assignees':[assignees],
+                 'labels':[labels]}
+    response = requests.post(base_,
+                             data=json.dumps(data_dict),
+                             headers=headers,
+                             )
+    if response.status_code != 201:
+        raise ValueError('Ticket creation failed')
+    return
+
+
+if __name__ == '__main__':
+    payload = os.environ['INPUT_PAYLOAD']
+    title = payload['title']
+    body = payload['body']
+    assignee = payload['assignee']
+    milestone = payload['milestone']
+    labels = payload['labels']
+    repo = 'procstar'
+    create_issue(title, body, assignee, milestone, labels, repo)
