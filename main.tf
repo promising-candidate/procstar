847adacae47e6938c555bddefe3f047c89d59809
promising-candidate <41706714+promising-candidate@users.noreply.github.com>
2026-03-25 17:54:07 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "rg_name" {
+  description = "The name of the resource group"
+  type        = string
+  nullable = false
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-student-${var.rg_name}"
+  location = "westus2"
+}
